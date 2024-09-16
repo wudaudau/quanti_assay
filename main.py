a2ef55cb350b235692101c8d64601c9457db6c2c
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-09-16 22:30:06 +0200

    Add main.py to run the app and insert data from CSV
    This commit adds a new file, main.py, which contains the main function to run the app. It also includes the functionality to insert data from a CSV file to the database. The main function accepts command line arguments to determine whether to run the app or insert data. If the command is "run", it calls the run_app() function from app.py. If the command is "insert", it calls the insert_data_from_csv() function from data_insertion.py, passing the CSV file path as an argument.

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+from src.app import run_app
+from src.data_insertion import insert_data_from_csv
+
+
+
+def main():
+    """
+    Main function to run the app.
+    If we do the following in the CLI:
+    - python main.py run -> it runs the app by calling run_app() from app.py
+    - python main.py insert data.csv -> it inserts data from data.csv to the database by calling insert_data_from_csv() from data_insertion.py
+    """
+    if sys.argv[1] == "run":
+        run_app()
+    elif sys.argv[1] == "insert":
+        insert_data_from_csv(sys.argv[2])
+    else:
+        print("Invalid command.")
+        print("Please use the following commands:")
+        print(f"- python main.py run")
+        print(f"- python main.py insert data.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/app.py
@@ -0,0 +1,8 @@
+
+# main function to run the app
+def run_app():
+    print("Hi, it's app.py!")
+
+
+if __name__ == '__main__':
+    run_app()
