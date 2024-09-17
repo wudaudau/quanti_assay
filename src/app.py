690f975820260c00677ed2358c7fbe77332168f3
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-09-17 11:08:51 +0200

    Refactor app.py and add create_db function
    This commit refactors the app.py file by adding a new function called create_db to the db_utils module. The create_db function is responsible for creating a SQLite database. Additionally, the run_app function in app.py now calls the create_db function before printing a welcome message. This commit improves the organization and functionality of the code.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,56 @@
 
+from src.db_utils import create_db
+
 # main function to run the app
 def run_app():
-    print("Hi, it's app.py!")
+    create_db()
+    print("Hi, it's the Quanti app!")
+
+    assay_id = ask_assay_name()
+
+
+
+# The following are the low-level functions
+
+
+def ask_assay_name() -> int:
+    """
+    This function asks the user to select an assay name.
+    If no data is available, go to add_assay_name().
+    If data is available, show the list of assay names and ask the user to select one.
+        if the input is not in the list, ask the user to select again.
+        else, return the assay_id.
+    """
+    assays = ["A", "B", "C"] # None # get_assay_names_from_db() # get assay names from the database
+
+    if not assays:
+        add_assay_name()
+    else:
+        print("Please select an assay name:")
+        for i, assay in enumerate(assays):
+            print(f"{i+1}. {assay}")
+
+        while True:
+            try:
+                assay_id = int(input("Enter the number of the assay name: "))
+                if assay_id < 1 or assay_id > len(assays):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+        return assay_id
+    
+def add_assay_name():
+    """
+    This function asks the user to add an assay name. TODO: we will add analytes as well.
+    Then insert the assay name into the database.
+    """
+    assay_name = input("Enter the assay name: ")
+    # insert_assay_name_to_db(assay_name)
+    print(f"{assay_name} has been added to the database.")
+
+
 
 
 if __name__ == '__main__':
---
+++ src/db_utils.py
@@ -0,0 +1,13 @@
+"""
+This module contains utility functions for interacting with the SQLite database.
+"""
+
+import sqlite3
+
+
+def create_db():
+    # conn = 
+    # cursor = 
+    # conn.commit()
+    # conn.close()
+    print("This will create a database.")
