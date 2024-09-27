c156b9c449e174e1137c07b9048abaf1cf2285d6
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-09-27 16:06:58 +0200

    REF: Update schema to prevent the db creating issue.
    - Use the schema for the updated kit_item related tables.
    - Updated the schema.sql file to set the unique constraint on the kit_cat_number column in the kit table.
    - Added a new storage table to the schema.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,17 +1,23 @@
+CREATE TABLE manufacture
+(
+  id   INTEGER NOT NULL,
+  name TEXT    NULL    ,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE storage
+(
+  id          INTEGER NOT NULL,
+  name        TEXT    NULL    ,
+  description TEXT    NULL    ,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE kit
 (
   id             INTEGER NOT NULL,
-  kit_cat_number         NOT NULL DEFAULT Unique,
-  name                   NULL    ,
-  manufacture_id INTEGER NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (manufacture_id) REFERENCES manufacture (id)
-);
-
-CREATE TABLE kit_item
-(
-  id             INTEGER NOT NULL,
-  cat_number             NULL    ,
+  kit_cat_number TEXT    NOT NULL UNIQUE,
+  name           TEXT    NULL    ,
   manufacture_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (manufacture_id) REFERENCES manufacture (id)
@@ -27,9 +33,3 @@ CREATE TABLE kits_kit_items
   FOREIGN KEY (kit_item_id) REFERENCES kit_item (id)
 );
 
-CREATE TABLE manufacture
-(
-  id   INTEGER NOT NULL,
-  name         NULL    ,
-  PRIMARY KEY (id)
-);
