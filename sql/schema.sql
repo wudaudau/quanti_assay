54e5200092b8afdbbfca527a9214c4cb5156d735
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-09-30 22:21:32 +0200

    REF: Refactor schema.sql to add tables for analyte and std_analyte

--- sql/schema.sql
+++ sql/schema.sql
@@ -37,3 +37,40 @@ CREATE TABLE kits_kit_items
   FOREIGN KEY (kit_item_id) REFERENCES kit_item (id)
 );
 
+
+
+
+
+
+
+
+
+
+
+
+-- It can see how many isoform we measure for the same analyte. It can also for selecting the assay.
+CREATE TABLE analyte
+(
+  id   INTEGER NOT NULL,
+  name TEXT    NULL     UNIQUE,
+  PRIMARY KEY (id)
+);
+
+-- the standard name to unify in the system. We still need to distingrish between IL-8 and IL-8(HA)
+CREATE TABLE std_analyte
+(
+  id   INTEGER NOT NULL,
+  name TEXT    NULL     UNIQUE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE analyte_std_analyte
+(
+  id             INTEGER NOT NULL,
+  analyte_id     INTEGER NOT NULL,
+  std_analyte_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (analyte_id) REFERENCES analyte (id),
+  FOREIGN KEY (std_analyte_id) REFERENCES std_analyte (id)
+  UNIQUE (analyte_id, std_analyte_id)
+);
