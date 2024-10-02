cdcf29f9bddce30ee08c78074e7eadeb63e93b5b
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-10-02 16:19:31 +0200

    REF: Refactor item_lot and sd_initial_conc tables
    - move expiration_data column from sd_inital_conc to item_lot

--- sql/schema.sql
+++ sql/schema.sql
@@ -152,19 +152,19 @@ CREATE TABLE item_lot
   id         INTEGER NOT NULL,
   item_id    INTEGER NOT NULL,  -- UNIQUE(item_id, lot_number)
   lot_number TEXT    NOT NULL,  -- UNIQUE(item_id, lot_number)
+  expiration_date TEXT    NULL    ,
   PRIMARY KEY (id),
   FOREIGN KEY (item_id) REFERENCES kit_item (id),
   UNIQUE(item_id, lot_number)
 );
 
-CREATE TABLE sd_inital_conc
+CREATE TABLE sd_initial_conc
 (
   id              INTEGER NOT NULL,
   item_lot_id     INTEGER NOT NULL,  -- UNIQUE(item_lot_id, analyte_id)
   analyte_id      INTEGER NOT NULL,  -- UNIQUE(item_lot_id, analyte_id)
   conc            NUMERIC NULL    ,
   unit            TEXT    NULL    ,
-  expiration_date TEXT    NULL    ,
   PRIMARY KEY (id),
   FOREIGN KEY (analyte_id) REFERENCES analyte (id),
   FOREIGN KEY (item_lot_id) REFERENCES item_lot (id),
