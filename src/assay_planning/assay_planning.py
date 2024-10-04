b129b19251586b0cca40859c91e9aab32cb20b60
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-10-04 12:40:35 +0200

    REF: Move get_or_insert_assay_type(), get_or_insert_assay(), and add_assay_from_file() from db_utils.py to assay_planning.py

--- src/assay_planning/assay_planning.py
+++ src/assay_planning/assay_planning.py
@@ -3,6 +3,7 @@ For the moment, we implement the db-related functions here.
 """
 
 import sqlite3
+import csv
 
 from src.core.db_utils import check_exists
 from src.core.db_utils import get_or_insert_species
@@ -31,3 +32,62 @@ def get_or_insert_project(db_path, project_name:str, species:str, description:st
     conn.close()
 
     return project_id
+
+
+def get_or_insert_assay_type(db_path, assay_type_name:str):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    row = check_exists(db_path, "assay_type", {"name": assay_type_name})
+
+    if row:
+        assay_type_id = row[0]
+    else:
+        cursor.execute("INSERT INTO assay_type (name) VALUES (?);", (assay_type_name,))
+        assay_type_id = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return assay_type_id
+
+def get_or_insert_assay(db_path, assay_name:str, species:str, assay_type:str):
+    species_id = get_or_insert_species(db_path, species)
+    assay_type_id = get_or_insert_assay_type(db_path, assay_type)
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    row = check_exists(db_path, "assay", {"name": assay_name})
+
+    if row:
+        assay_id = row[0]
+    else:
+        cursor.execute("INSERT INTO assay (name, species_id, assay_type_id) VALUES (?,?,?);", 
+                    (assay_name, species_id, assay_type_id))
+        assay_id = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return assay_id
+
+def add_assay_from_file(db_path): # TODO: Do we still need this function?
+        
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    file_path = "data/initial_data_for_the_database/assay.csv"
+
+    with open(file_path, "r") as f:
+        reader = csv.DictReader(f, delimiter=";")
+        for row in reader:
+            assay_name = row["name"]
+            species = row["species"]
+            assay_type = row["assay_type"]
+            assay_id = get_or_insert_assay(db_path, assay_name, species, assay_type)
+
+    conn.commit()
+    conn.close()
+
+    # TODO: get_or_insert_project_assay()
--- src/core/db_utils.py
+++ src/core/db_utils.py
@@ -262,63 +262,6 @@ def get_or_insert_species(db_path, species_name:str):
 
     return species_id
 
-def get_or_insert_assay_type(db_path, assay_type_name:str):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    row = check_exists(db_path, "assay_type", {"name": assay_type_name})
-
-    if row:
-        assay_type_id = row[0]
-    else:
-        cursor.execute("INSERT INTO assay_type (name) VALUES (?);", (assay_type_name,))
-        assay_type_id = cursor.lastrowid
-
-    conn.commit()
-    conn.close()
-
-    return assay_type_id
-
-def get_or_insert_assay(db_path, assay_name:str, species:str, assay_type:str):
-    species_id = get_or_insert_species(db_path, species)
-    assay_type_id = get_or_insert_assay_type(db_path, assay_type)
-
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    row = check_exists(db_path, "assay", {"name": assay_name})
-
-    if row:
-        assay_id = row[0]
-    else:
-        cursor.execute("INSERT INTO assay (name, species_id, assay_type_id) VALUES (?,?,?);", 
-                    (assay_name, species_id, assay_type_id))
-        assay_id = cursor.lastrowid
-
-    conn.commit()
-    conn.close()
-
-    return assay_id
-
-def add_assay_from_file(db_path):
-        
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    file_path = "data/initial_data_for_the_database/assay.csv"
-
-    with open(file_path, "r") as f:
-        reader = csv.DictReader(f, delimiter=";")
-        for row in reader:
-            assay_name = row["name"]
-            species = row["species"]
-            assay_type = row["assay_type"]
-            assay_id = get_or_insert_assay(db_path, assay_name, species, assay_type)
-
-    conn.commit()
-    conn.close()
-
-    
 
 
 
@@ -337,6 +280,7 @@ def add_assay_from_file(db_path):
 
 
 def get_or_insert_assays_analytes(db_path, assay_name:str, analyte_name:str, spot:int, opt_analyte_name:str):
+    from src.assay_planning.assay_planning import get_or_insert_assay
     assay_id = get_or_insert_assay(db_path, assay_name, None, None)
     analyte_id = get_or_insert_analyte(db_path, analyte_name)
 
