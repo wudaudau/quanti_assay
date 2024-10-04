90fb15aaa3d9cd19b65ac8cd5c4761ce693f6865
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-10-04 11:05:22 +0200

    REF: Add project_assay junction table to assay_planning_schema.sql

--- sql/assay_planning_schema.sql
+++ sql/assay_planning_schema.sql
@@ -10,3 +10,15 @@ CREATE TABLE project
   FOREIGN KEY (species_id) REFERENCES species (id)
 );
 
+
+-- Junction table to assign assays to projects
+CREATE TABLE project_assay
+(
+  id         INTEGER NOT NULL,
+  project_id INTEGER NOT NULL,  -- UNIQUE (project_id, assay_id)
+  assay_id   INTEGER NOT NULL,  -- UNIQUE (project_id, assay_id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (project_id) REFERENCES project (id),
+  FOREIGN KEY (assay_id) REFERENCES assay (id),
+  UNIQUE (project_id, assay_id)
+);
