99d135758f6e5e48ce4d3968d6777a6f989fd3b0
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-10-16 16:44:28 +0200

    REF: Refactor test_create_db() and test_get_table_names() to adapt the schema change

--- tests/unit/test_core/test_db_utils.py
+++ tests/unit/test_core/test_db_utils.py
@@ -29,14 +29,13 @@ class TestDBUtils(unittest.TestCase):
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         table_names = cursor.fetchall()
 
-        self.assertEqual(len(table_names), 15)
+        self.assertEqual(len(table_names), 16)
         self.assertEqual(table_names[0][0], "species")
         self.assertEqual(table_names[1][0], "project")
         self.assertEqual(table_names[6][0], "analyte")
         self.assertEqual(table_names[8][0], "assays_analytes")
         self.assertEqual(table_names[9][0], "manufacture")
-        self.assertEqual(table_names[13][0], "item_lot")
-
+        self.assertEqual(table_names[14][0], "item_lot")
 
         conn.close()
         
@@ -45,13 +44,13 @@ class TestDBUtils(unittest.TestCase):
         create_db(self.db_path)
 
         table_names = get_table_names(self.db_path)
-        self.assertEqual(len(table_names), 15)
+        self.assertEqual(len(table_names), 16)
         self.assertEqual(table_names[0], "species")
         self.assertListEqual(table_names, ["species",
                                            "project", "assay_type", "assay", "sample_type", "projects_assays",
                                            "analyte", "analyte_mapping",
                                            "assays_analytes",
-                                           "manufacture", "storage", "kit_item", "kits_kit_items",
+                                           "manufacture", "storage", "quanti_item_type", "product", "kits_items",
                                            "item_lot", "sd_initial_conc",
                                            ])
 
