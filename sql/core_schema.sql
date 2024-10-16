0cba9ec565901b5378bd34b4b089177858dc6a58
wudaudaugit config --global user.name wudaudaugit config --global user.name wudaudau <wudaudau@gmail.com>
2024-10-16 11:05:03 +0200

    REF: Move kit-related tables from core_schema.sql to kit_order_schema.sql
    - The tables are: manufacture, storage, kit_item, kits_kit_items
    - TODO: We need to create a kit table and reform the kit_items table

--- sql/core_schema.sql
+++ sql/core_schema.sql
@@ -23,93 +23,9 @@ CREATE TABLE species
 
 
 
-CREATE TABLE manufacture
-(
-  id   INTEGER NOT NULL,
-  name TEXT    NULL    ,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE storage
-(
-  id          INTEGER NOT NULL,
-  name        TEXT    NOT NULL    ,
-  description TEXT    NULL    ,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE kit_item
-(
-  id             INTEGER NOT NULL,
-  kit_cat_number TEXT    NOT NULL UNIQUE,
-  name           TEXT    NULL    ,
-  manufacture_id INTEGER NOT NULL,
-  storage_id     INTEGER NOT NULL,
-  description    TEXT    NULL    ,
-  PRIMARY KEY (id),
-  FOREIGN KEY (manufacture_id) REFERENCES manufacture (id),
-  FOREIGN KEY (storage_id) REFERENCES storage (id)
-);
-
-
-CREATE TABLE kits_kit_items
-(
-  id          INTEGER NOT NULL,
-  kit_id      INTEGER NOT NULL,
-  kit_item_id INTEGER NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (kit_id) REFERENCES kit (id),
-  FOREIGN KEY (kit_item_id) REFERENCES kit_item (id)
-);
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Mostly are 
-CREATE TABLE item_lot
-(
-  id         INTEGER NOT NULL,
-  item_id    INTEGER NOT NULL,  -- UNIQUE(item_id, lot_number)
-  lot_number TEXT    NOT NULL,  -- UNIQUE(item_id, lot_number)
-  expiration_date TEXT    NULL    ,
-  PRIMARY KEY (id),
-  FOREIGN KEY (item_id) REFERENCES kit_item (id),
-  UNIQUE(item_id, lot_number)
-);
-
-CREATE TABLE sd_initial_conc
-(
-  id              INTEGER NOT NULL,
-  item_lot_id     INTEGER NOT NULL,  -- UNIQUE(item_lot_id, analyte_id)
-  analyte_id      INTEGER NOT NULL,  -- UNIQUE(item_lot_id, analyte_id)
-  conc            NUMERIC NULL    ,
-  unit            TEXT    NULL    ,
-  PRIMARY KEY (id),
-  FOREIGN KEY (analyte_id) REFERENCES analyte (id),
-  FOREIGN KEY (item_lot_id) REFERENCES item_lot (id),
-  UNIQUE(item_lot_id, analyte_id)
-);
 
